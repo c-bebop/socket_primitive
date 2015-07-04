15228c58af38d4566c482d8ae46408371e6b717f
flowmoe <sir.psycho@gmx.net>
2015-07-04 13:14:51 +0200

    revised data type memory allocation c++ application and added documentation

--- dataype_primitive_c++_application/main.cpp
+++ dataype_primitive_c++_application/main.cpp
@@ -1,24 +1,100 @@
 #include <iostream>
 
+/**
+ * Prints the size of the transferred value in bytes as follows:
+ *
+ * Size of 'type_name': X byte
+ *
+ * Where X is the size that is returned by the function sizeof(T).
+ *
+ * @param   type_name        The name of the type.
+ */
 template <typename T>
-void print_size(T type, std::string name)
+void print_size(std::string type_name)
 {
-    std::cout << "Size of " << name << ": " << sizeof(type) << " byte" << std::endl;
+    std::cout << "Size of " << type_name << ": " << sizeof(T) << " byte" << std::endl;
 }
 
-int main() {
+/**
+ * Prints out the following integer types:
+ *
+ * - signed char
+ * - unsigned char
+ * - char
+ * - short
+ * - short int
+ * - unsigned short int
+ * - int
+ * - unsigned int
+ * - long int
+ * - unsigned long int
+ * - long long int
+ * - unsigned long long int
+ */
+void print_integer_types()
+{
+    print_size<signed char>            ("signed char");
+    print_size<unsigned char>          ("unsigned char");
+    print_size<char>                   ("char");
+    print_size<short>                  ("short");
+    print_size<short int>              ("short int");
+    print_size<unsigned short int>     ("unsigned short int");
+    print_size<int>                    ("int");
+    print_size<unsigned int>           ("unsigned int");
+    print_size<long int>               ("long int");
+    print_size<unsigned long int>      ("unsigned long int");
+    print_size<long long int>          ("long long int");
+    print_size<unsigned long long int> ("unsigned long long int");
+}
 
-    char    character { 'a' };
-    int     integer_standard { 1 };
-    int64_t integer_64 { 1 };
-    float   floating_point { 1.0f };
-    double  floating_point_double_precision { 1.0 };
+/**
+ * Prints out the following real number types:
+ *
+ * - float
+ * - double
+ * - long double
+ */
+void print_real_number_types()
+{
+    print_size<float>       ("float"                       );
+    print_size<double>      ("double");
+    print_size<long double> ("long double");
+}
 
-    print_size<char>(character, "character (char)");
-    print_size<int>(integer_standard, "standard integer (int)");
-    print_size<int64_t >(integer_64, "64-bit integer (int64_t)");
-    print_size<float>(floating_point, "floating point (float)");
-    print_size<double>(floating_point_double_precision, "floating point with double precision (double)");
+/**
+ * This application prints all listed primitive data types of the
+ * GNU C Reference Manual [1] excluding the complex number types
+ * with information on their allocated memory:
+ *
+ * Integer Types:
+ * - signed char
+ * - unsigned char
+ * - char
+ * - short
+ * - short int
+ * - unsigned short int
+ * - int
+ * - unsigned int
+ * - long int
+ * - unsigned long int
+ * - long long int
+ * - unsigned long long int
+ *
+ * Real Number Types:
+ * - float
+ * - double
+ * - long double
+ *
+ * [1] Rothwell, Trevis, & Youngman, James. 2015. The GNU C Reference Manual.
+ * http://www.gnu.org/software/gnu-c-manual/gnu-c-manual.pdf . Free Software
+ * Foundation, Inc. [Online. Accessed 1st July 2015].
+ *
+ * @author Florian Willich
+ */
+int main()
+{
+    print_integer_types();
+    print_real_number_types();
 
     return 0;
 };
